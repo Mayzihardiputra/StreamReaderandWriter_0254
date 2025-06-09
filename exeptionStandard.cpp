74aa58adff0b7a2e927ed878b7c5ae8b553974d5
Mayzihardiputra <eziee496@gmail.com>
2025-06-09 18:57:20 +0700

    try

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -8,4 +8,11 @@ using namespace std;
 int main()
 {
     cout << "Awal Program" << endl; // penanda 1
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(4) << endl;
+        // memanggil array elemen ke5
+    }
 }
