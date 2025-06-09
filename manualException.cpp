a06bcf44e2b144ef15ec4a555263a02f53415b32
Mayzihardiputra <eziee496@gmail.com>
2025-06-09 18:49:01 +0700

    commit

--- manualException.cpp
+++ manualException.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw("halo"); // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+        }
+}
